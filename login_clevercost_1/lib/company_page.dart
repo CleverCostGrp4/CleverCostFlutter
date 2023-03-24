dca18cb34b1e36ca6e9cba0ede74100fffc59c48
Kevin Matin <kevin-matin@hotmail.com>
2023-03-24 12:27:50 +0100

    Added Company outline

---
+++ login_clevercost_1/lib/company_page.dart
@@ -0,0 +1,133 @@
+//ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class CompanyPage extends StatefulWidget {
+  const CompanyPage({super.key});
+
+  @override
+  State<CompanyPage> createState() => _CompanyPageState();
+}
+
+class _CompanyPageState extends State<CompanyPage> {
+  //Continue to Dashboard
+  continueToDash() {
+    //Navigation to Dashboard
+  }
+
+  bool rememberCompany = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              //Top Logo
+              Image.asset('lib/images/clevercost_company_page.png'),
+
+              SizedBox(height: 20),
+
+              //Company Listings with press
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30), //Spacing
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30), //Spacing
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30), //Spacing
+
+              //Access this company by default when I log into my clevercost account
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  onTap: () {
+                    setState(() {
+                      rememberCompany = !rememberCompany;
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      Checkbox(
+                        value: rememberCompany,
+                        onChanged: (newValue) {
+                          setState(() {
+                            rememberCompany = newValue!;
+                          });
+                        },
+                      ),
+                      Text(
+                        "Access this company by default \nwhen I log into my Clever Cost account.",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20), //Spacing
+
+              //Continue Button
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: GestureDetector(
+                  onTap: continueToDash,
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Color(0xff347dfe),
+                      borderRadius: BorderRadius.circular(6),
+                      border: Border.all(color: Colors.grey),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Continue',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- login_clevercost_1/lib/login_page.dart
+++ login_clevercost_1/lib/login_page.dart
@@ -1,5 +1,6 @@
 //ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'company_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -9,6 +10,21 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  //Email-Controller
+  final _emailController = TextEditingController();
+  //Password-Controller
+  final _passwordController = TextEditingController();
+
+  var companyRoute = MaterialPageRoute(
+    builder: (BuildContext context) => CompanyPage(),
+  );
+
+  signIn() {
+    Navigator.push(context, companyRoute);
+  }
+
+  bool rememberMe = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,165 +32,191 @@ class _LoginPageState extends State<LoginPage> {
       body: SafeArea(
         // ignore: prefer_const_literals_to_create_immutables
         child: Center(
-          child: Column(
-            children: [
-              //Clevercost Logo
-              Image.asset('lib/images/clevercost_login.png'),
-              SizedBox(height: 5),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                //Clevercost Logo
+                Image.asset('lib/images/clevercost_login.png'),
 
-              //Account e-mail
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Account e-mail',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Color(0xff43425d),
-                    ),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 10), //Spacing
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[50],
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(6),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 30), //Spacing
-
-              //Password
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Password',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Color(0xff43425d),
-                    ),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 10), //Spacing
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[50],
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(6),
-                  ),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 30), //Spacing
-
-              //Remember me on this device
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Remember me on this device',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 16,
-                      color: Color(0xff43425d),
-                    ),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 30),
-
-              //Log in
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Color(0xff347dfe),
-                    borderRadius: BorderRadius.circular(6),
-                    border: Border.all(color: Colors.grey),
-                  ),
-                  child: Center(
+                //Account e-mail
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
                     child: Text(
-                      'Log in',
+                      'Account e-mail',
                       style: TextStyle(
-                        color: Colors.white,
                         fontWeight: FontWeight.bold,
                         fontSize: 16,
+                        color: Color(0xff43425d),
                       ),
                     ),
                   ),
                 ),
-              ),
 
-              //Sign up
+                SizedBox(height: 8), //Spacing
 
-              SizedBox(height: 20),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 2,
-                        color: Colors.grey[400],
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[50],
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: TextField(
+                      controller: _emailController, //Email-Controller
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        hintText: 'Email',
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text('or'),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 2,
-                        color: Colors.grey[400],
+                  ),
+                ),
+                SizedBox(height: 30), //Spacing
+
+                //Password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Password',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Color(0xff43425d),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
 
-              SizedBox(height: 20),
+                SizedBox(height: 8), //Spacing
 
-              Text(
-                'Sign up',
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 16,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[50],
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: TextField(
+                      controller: _passwordController, //Password-Controller
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        hintText: 'Password',
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ],
+
+                SizedBox(height: 10), //Spacing
+
+                //Remember me on this device
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () {
+                      setState(() {
+                        rememberMe = !rememberMe;
+                      });
+                    },
+                    child: Row(
+                      children: [
+                        Checkbox(
+                          value: rememberMe,
+                          onChanged: (newValue) {
+                            setState(() {
+                              rememberMe = newValue!;
+                            });
+                          },
+                        ),
+                        Text(
+                          "Remember me on this device",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                //Log in
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: GestureDetector(
+                    onTap: signIn,
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Color(0xff347dfe),
+                        borderRadius: BorderRadius.circular(6),
+                        border: Border.all(color: Colors.grey),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Log in',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                //Sign up
+
+                SizedBox(height: 20),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 2,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text('or'),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 2,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                Text(
+                  'Sign up',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16,
+                  ),
+                ),
+
+                SizedBox(height: 34),
+              ],
+            ),
           ),
         ),
       ),
